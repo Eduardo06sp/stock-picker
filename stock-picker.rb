75ab557f6138a56dd66b394fd7c7670a58d6656c
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-02-04 22:23:53 -0800

    Initial commit

---
+++ stock-picker.rb
@@ -0,0 +1,17 @@
+def stock_picker(prices)
+  best_buy = prices[0]
+  best_sell = prices[1]
+  best_profit = best_sell - best_buy
+
+  buy_price
+  sell_price
+  profit
+
+  buy_index = 0
+  sell_index = 1
+
+  while buy_price != prices.last - 1
+    buy_index += 1
+    sell_index += 1
+  end
+end
